0ad95359be957f337d0d7c5399855b07fa9338b3
Phani Adabala <adabal.phani@gmail.com>
2021-03-27 23:30:48 +0530

    version 0.0.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="ShiningArmor", 
-    version="0.0.3",
+    version="0.0.4",
     author="Phani Adabala",
     author_email="adabala.phani@gmail.com",
     description="A collection of python functions for BOTs",
