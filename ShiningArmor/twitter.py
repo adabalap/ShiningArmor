a54555733c83dd8265517316741bb0ca5e4b3e62
Phani Adabala <adabal.phani@gmail.com>
2021-03-27 23:28:13 +0530

    Updated code to tweet messages in thread format.

--- ShiningArmor/twitter.py
+++ ShiningArmor/twitter.py
@@ -2,58 +2,55 @@ import json
 import tweepy
 
 
-
-
 def tokens(tokens_file):
     # Get Twitter tokens
-
     try:
-        f =  open(tokens_file, mode='r')
+        f = open(tokens_file, mode='r')
         tokens = json.load(f)
-
+        f.close()
     except FileNotFoundError as err:
         raise err
-
     except IOError as err:
         raise err
 
-    finally:
-        f.close()
-
     return tokens
-
-
 
 
 def auth(tokens):
     # tokens can be found under "Apps > RainBowDashBOT"
     try:
-        auth = tweepy.OAuthHandler(tokens['consumer_api_key'], \
-                tokens['consumer_api_key_secret'])
+        auth = tweepy.OAuthHandler(tokens['consumer_api_key'],
+                                   tokens['consumer_api_key_secret'])
 
-        auth.set_access_token(tokens['access_token'], \
-                tokens['access_token_secret'])
+        auth.set_access_token(tokens['access_token'],
+                              tokens['access_token_secret'])
 
         api = tweepy.API(auth)
 
-    except Error as err:
+    except Exception as err:
         raise err
 
     return api
 
 
-
-
 def tweet(api, tweet):
     # post the tweet on twitter
-
     status = 0
 
     try:
-        api.update_status(tweet)
+        if isinstance(tweet, str):
+            tweet = tweet.split('\n')
+
+        orig_tweet = api.update_status(status=tweet[0])
+
+        for i in tweet[1:]:
+            reply_tweet = api.update_status(status=i,
+                                            in_reply_to_status_id=orig_tweet.id,
+                                            auto_populate_reply_metadata=True)
+            orig_tweet = reply_tweet
 
     except tweepy.error.TweepError as err:
-        raise err
         status = 1
+        raise err
 
-    return status 
+    return status
