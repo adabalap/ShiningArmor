849f0b288e6dacba0f0c109172a51e02369915cf
Phani Adabala <adabal.phani@gmail.com>
2021-03-28 14:20:29 +0530

    new version v0.0.5
    updated logic for tweet(api, tweet) function:
    1. tweet threads
    2. handle plain string messages or list messages (as threads)

--- ShiningArmor/twitter.py
+++ ShiningArmor/twitter.py
@@ -3,29 +3,29 @@ import tweepy
 
 
 def tokens(tokens_file):
-    # Get Twitter tokens
+    # Get Twitter twitter_tokens
     try:
         f = open(tokens_file, mode='r')
-        tokens = json.load(f)
+        twitter_tokens = json.load(f)
         f.close()
     except FileNotFoundError as err:
         raise err
     except IOError as err:
         raise err
 
-    return tokens
+    return twitter_tokens
 
 
-def auth(tokens):
+def auth(t_tokens):
     # tokens can be found under "Apps > RainBowDashBOT"
     try:
-        auth = tweepy.OAuthHandler(tokens['consumer_api_key'],
-                                   tokens['consumer_api_key_secret'])
+        t_auth = tweepy.OAuthHandler(t_tokens['consumer_api_key'],
+                                     t_tokens['consumer_api_key_secret'])
 
-        auth.set_access_token(tokens['access_token'],
-                              tokens['access_token_secret'])
+        t_auth.set_access_token(t_tokens['access_token'],
+                                t_tokens['access_token_secret'])
 
-        api = tweepy.API(auth)
+        api = tweepy.API(t_auth)
 
     except Exception as err:
         raise err
@@ -39,15 +39,15 @@ def tweet(api, tweet):
 
     try:
         if isinstance(tweet, str):
-            tweet = tweet.split('\n')
-
-        orig_tweet = api.update_status(status=tweet[0])
-
-        for i in tweet[1:]:
-            reply_tweet = api.update_status(status=i,
-                                            in_reply_to_status_id=orig_tweet.id,
-                                            auto_populate_reply_metadata=True)
-            orig_tweet = reply_tweet
+            orig_tweet = api.update_status(status=tweet)
+        else:
+            # multi-line message - create twitter thread
+            orig_tweet = api.update_status(status=tweet[0])
+            for i in tweet[1:]:
+                reply_tweet = api.update_status(status=i,
+                                                in_reply_to_status_id=orig_tweet.id,
+                                                auto_populate_reply_metadata=True)
+                orig_tweet = reply_tweet
 
     except tweepy.error.TweepError as err:
         status = 1
